0a0fb2eca2c702352e7a8cc7df31f399d9d6463f
Nariman324 <119915847+Nariman324@users.noreply.github.com>
2022-12-13 16:42:29 +0600

    Add files via upload

---
+++ task19/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+   
+   Console.WriteLine("введите пятизначное число");
+            int number = int.Parse(Console.ReadLine());
+            int pal = 0, num = number;
+            while (number > 0) {pal = pal * 10 + number % 10; number /= 10;}
+            Console.WriteLine(num == pal ? "палиндром" : "не палиндром");
